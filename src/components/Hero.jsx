6349ca8f27c5b9ae43dae365e7fd5f9509f66c56
roconn <roconn25@gmail.com>
2023-04-25 20:49:43 +0100

    added placeholder content to hero, styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,67 @@ const Section = styled.div`
   height: 100vh;
   background-color: #fbfffe;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  `
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  `
+const ASide = styled.div`
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+  `
+const BSide = styled.div`
+    flex: 3;
+  `
+const Title = styled.h1`
+    font-size: 36pt;
+  `
+const Description = styled.p`
+    display: flex;
+    align-items: center;
+    /* gap: 10px; */
+  `
+const HeroImage = styled.img`
+    height: 40%;
+  `
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  background-color: #faa916;
+  color: black;
+  cursor: pointer;
+  border: none;
+  border-radius: 10px;
+  font-family: inherit;
+  /* font-size: 1em; */
   `
 
 const Hero = () => {
     return (
         <Section>
             <Navbar />
+            <Container>
+                <ASide>
+                    <Title>Hello There</Title>
+                    <Description>I am the person doing the thing</Description>
+                    <Button>Press Me</Button>
+                </ASide>
+                <BSide>
+                    {/* 3d model pending */}
+                    <HeroImage src="./public/images/rochimselfsmolroundedpng-min.png" />
+                </BSide>
+            </Container>
         </Section>
     )
 }
