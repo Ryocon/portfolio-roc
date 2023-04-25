8c0c7bfd6dde2d0370d295845aa7030b1aec056b
roconn <roconn25@gmail.com>
2023-04-25 20:04:19 +0100

    updated styled components, added global font

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import './App.css'
+import './App.css'
 import styled from 'styled-components'
 import Hero from './components/Hero'
 import About from './components/About'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,80 @@
 import React from "react";
 import styled from "styled-components";
-import {BiSearchAlt} from "react-icons/bi"
+import { BiSearchAlt } from "react-icons/bi";
 
 const Section = styled.div`
-    display: flex;
-    justify-content: center;
-  `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Container = styled.div`
-    width: 80%;
-    background-color: #faa916;
-`
+  width: 80%;
+  /* background-color: #faa916; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0px;
+`;
 const Links = styled.div`
-    
-`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
 const Logo = styled.img`
-width: 10%;
-    
-`
+  width: 10%;
+`;
 const List = styled.ul`
-    
-`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+`;
 const ListItem = styled.li`
-    
-`
+  cursor: pointer;
+`;
 const Icons = styled.div`
-    
-`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
 const Icon = styled.div`
-    
-`
+  width: 20px;
+  cursor: pointer;
+`;
 const Button = styled.button`
-    
-`
-
+  width: 100px;
+  padding: 10px;
+  background-color: #faa916;
+  color: black;
+  cursor: pointer;
+  border: none;
+  border-radius: 10px;
+  font-family: inherit;
+  /* font-size: 1em; */
+`;
 
 const Navbar = () => {
-    return (
-        <Section>
-            <Container>
-                <Links>
-                <Logo src="./public/images/roc.png" />
-                <List>
-                    <ListItem>Home</ListItem>
-                    <ListItem>About</ListItem>
-                    <ListItem>Projects</ListItem>
-                    <ListItem>Contact</ListItem>
-                </List>
-                </Links>
-                <Icons>
-                    <Icon><BiSearchAlt /></Icon>
-                    <Button>Get In Touch</Button>
-                </Icons>
-            </Container>
-        </Section>
-    )
-}
+  return (
+    <Section>
+      <Container>
+        <Links>
+          <Logo src="./public/images/roc.png" />
+          <List>
+            <ListItem>Home</ListItem>
+            <ListItem>About</ListItem>
+            <ListItem>Projects</ListItem>
+            <ListItem>Contact</ListItem>
+          </List>
+        </Links>
+        <Icons>
+          <Icon>
+            <BiSearchAlt size={24} />
+          </Icon>
+          <Button>Say Hi</Button>
+        </Icons>
+      </Container>
+    </Section>
+  );
+};
 
-export default Navbar
+export default Navbar;
