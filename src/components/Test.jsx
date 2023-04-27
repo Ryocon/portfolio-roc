7e80e70fbbe685e5aadbc01bdf6b1c0a8d559d41
roconn <roconn25@gmail.com>
2023-04-27 19:30:37 +0100

    test component updates

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100%;
+    scroll-snap-align: center;
+`
+
 
 const Test = () => {
     return (
-        <div>Test</div>
+        <Container>
+            <Canvas>
+                <OrbitControls enableZoom={false} autoRotate/>
+                <directionalLight position={ [ 3, 2, 1 ] } />
+            <mesh>
+            <torusGeometry args={[10, 3, 16, 100]} />
+            <meshStandardMaterial />
+            </mesh>
+            </Canvas>
+        </Container>
     )
 }
 
