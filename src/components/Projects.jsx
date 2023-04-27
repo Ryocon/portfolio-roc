ba88127db74ddbaeece20e81536b3e103a9654d4
roconn <roconn25@gmail.com>
2023-04-27 11:40:43 +0100

    components added in projects + data mapping

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,56 @@
 import React from "react";
 import styled from "styled-components";
 
+// ! Projects in the list
+const data = [
+    'Madchef',
+    'Alt-F4',
+    'MCU-deep-diver',
+    'Pokemon-API',
+    'Team-Profile-Gen'
+]
+
 const Section = styled.div`
   height: 100vh;
   background-color: #fbfffe;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+  `
+const Container = styled.div`
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+  `
+const ASide = styled.div`
+  flex: 1;
+  `
+const List = styled.ul`
+
+  `
+const ListItem = styled.li`
+  
+  `
+
+  const BSide = styled.div`
+  flex: 1;
   `
 
 const Projects = () => {
     return (
         <Section>
-            Projects
+            <Container>
+                <ASide>
+                    <List>
+                        {data.map((item) => (
+                            <ListItem key={item}>{item}</ListItem>
+                        ))}    
+                    </List>
+                </ASide>
+                <BSide>
+
+                </BSide>
+            </Container>
         </Section>
     )
 }
