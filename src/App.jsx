42ff4831e87626f961665d5bfb4e801601dabdf6
roconn <roconn25@gmail.com>
2023-04-27 15:29:08 +0100

    test component + threejs, fiber and drei added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Test from './components/Test'
 
 const Container = styled.div`
   height: 100vh;
@@ -24,6 +25,7 @@ function App() {
      <About />
      <Projects />
      <Contact />
+     <Test />
     </Container>
   )
 }
