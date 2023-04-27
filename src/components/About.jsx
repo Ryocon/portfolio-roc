96f1aa95bf12573dbaed658474afca121f6d9aec
roconn <roconn25@gmail.com>
2023-04-27 11:19:31 +0100

    added components to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,63 @@ import styled from "styled-components";
 
 const Section = styled.div`
   height: 100vh;
-  background-color: #6d676e;
+  background-color: #fbfffe;
   scroll-snap-align: center;
+  display: flex;
+  justify-content: center;
+  `
+const Container = styled.div`
+  height: 100%;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+  `
+const ASide = styled.div`
+    flex: 1;
+  `
+const BSide = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+  `
+const Title = styled.h1`
+    font-size: 36pt;
+  `
+const Description = styled.p`
+    display: flex;
+    align-items: center;
+    /* gap: 10px; */
+  `
+
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  background-color: #faa916;
+  color: black;
+  cursor: pointer;
+  border: none;
+  border-radius: 10px;
+  font-family: inherit;
+  /* font-size: 1em; */
   `
 
 const About = () => {
     return (
         <Section>
-            About
+            <Container>
+                <ASide>
+                    {/* 3d model pending */}
+                </ASide>
+                <BSide>
+                <Title>About me</Title>
+                    <Description>I am a dev</Description>
+                    <Button>Press Me</Button>
+                </BSide>
+            </Container>
         </Section>
     )
 }
