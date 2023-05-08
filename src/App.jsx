b4323bc26adc773bc49b76b7acaaa6466a34b5b2
roconn <roconn25@gmail.com>
2023-05-08 16:15:53 +0100

    updated gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Test from "./components/Test";
+// import Test from "./components/Test";
 import Spinner from "./components/Spinner";
 import { Suspense } from "react";
 
